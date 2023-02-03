a1c897e29e51174892fc6d773b560f3977288dad
fishluv <110374653+fishluv@users.noreply.github.com>
2023-02-02 20:55:39 -0800

    more cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default function Index() {
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link href="https://fonts.googleapis.com/css2?family=Changa+One&family=M+PLUS+1:wght@400;700;900&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1:wght@400;700;900&display=swap" rel="stylesheet" />
       </Head>
       <RandomizerApp />
     </>
